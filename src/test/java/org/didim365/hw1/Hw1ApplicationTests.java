8e386bf84d0d1b59992f213acc40d3f7c8583ab5
jungmin <parkjungmin@jungminui-MacBookPro.local>
2024-03-10 23:14:25 +0900

    init commit

---
+++ src/test/java/org/didim365/hw1/Hw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.didim365.hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
