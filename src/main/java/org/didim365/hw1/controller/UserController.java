23ab5c568bad1e1c394caca3ca47b3c62dfed555
jungmin <parkjungmin@jungminui-MacBookPro.local>
2024-03-11 13:16:17 +0900

    User controller, service 생성

--- src/main/java/org/didim365/hw1/controller/UserController.java
+++ src/main/java/org/didim365/hw1/controller/UserController.java
@@ -1,2 +1,25 @@
-package org.didim365.hw1.controller;public class UserController {
+package org.didim365.hw1.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.didim365.hw1.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(){
+        return "signup";
+    }
+
 }
--- src/main/java/org/didim365/hw1/service/UserService.java
+++ src/main/java/org/didim365/hw1/service/UserService.java
@@ -1,2 +1,7 @@
-package org.didim365.hw1.service;public class UserService {
+package org.didim365.hw1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
 }
