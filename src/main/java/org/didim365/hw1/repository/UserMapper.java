c85f48b49de9cada2192600af17b2b968fc4b463
jungmin <parkjungmin@jungminui-MacBookPro.local>
2024-03-12 17:54:40 +0900

    JPA 제거 MyBatis 추가

--- src/main/java/org/didim365/hw1/repository/UserMapper.java
+++ src/main/java/org/didim365/hw1/repository/UserMapper.java
@@ -1,11 +1,11 @@
 package org.didim365.hw1.repository;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.didim365.hw1.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+@Mapper
+public interface UserMapper {
     Optional<User> findByEmail(String email);
 }
-
--- src/main/java/org/didim365/hw1/service/UserService.java
+++ src/main/java/org/didim365/hw1/service/UserService.java
@@ -4,23 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.didim365.hw1.dto.user.SignUpRequestDto;
 import org.didim365.hw1.entity.User;
 import org.didim365.hw1.exception.user.DuplicateEmailException;
-import org.didim365.hw1.repository.UserRepository;
+import org.didim365.hw1.repository.UserMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
+    private final UserMapper userMapper;
     //private final PasswordEncoder passwordEncoder;
     public void signup(SignUpRequestDto signupRequestDto) {
         //String encodedPwd = passwordEncoder.encode(signupRequestDto.getPassword());
-        User user = new User(signupRequestDto);
+        //User user = new User(signupRequestDto);
         //user.update(encodedPwd);
 
-        userRepository.findByEmail(user.getEmail()).ifPresent((p) -> {
-            throw new DuplicateEmailException();
-        });
+//        userMapper.findByEmail(user.getEmail()).ifPresent((p) -> {
+//            throw new DuplicateEmailException();
+//        });
 
-        userRepository.save(user);
+        //userRepository.save(user);
     }
 }
