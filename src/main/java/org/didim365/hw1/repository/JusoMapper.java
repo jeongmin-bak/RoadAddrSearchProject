60501489f526e946b3499ad62d30d3534a25f227
jungmin <jmpark@didim365.com>
2024-03-19 03:30:41 +0900

    mapper 주석 추가

--- src/main/java/org/didim365/hw1/repository/JusoMapper.java
+++ src/main/java/org/didim365/hw1/repository/JusoMapper.java
@@ -9,9 +9,39 @@ import java.util.List;
 
 @Mapper
 public interface JusoMapper {
+    /**
+     *
+     * @param searchJuso 검색한 주소 값
+     * @return 저장 성공여부
+     */
     int saveSearchAddr(SearchJuso searchJuso);
+
+    /**
+     *
+     * @param memNb 회원번호
+     * @return 회원번호로 조회된 검색이력 리스트
+     */
     List<JusoSearchHisDto> findUserSearchHistory(String memNb);
+
+    /**
+     *
+     * @param memNb 회원번호
+     * @param searchAddr 도로명 주소
+     * @return 도로명 주소 검색 횟수
+     */
     Integer checkJusoSearchCount(String memNb, String searchAddr);
+
+    /**
+     * 이미 검색했던 도로명 주소가 있다면 +1 update
+     * @param memNb 회원번호
+     * @param searchAddr 도로명 주소
+     */
     void updateSearchCount(String memNb, String searchAddr);
+
+    /**
+     * 도로명 주소 검색 내역 삭제
+     * @param memNb 회원번호
+     * @param searchAddr 도로명 주소
+     */
     void deleteUserSearchHistory(String memNb, String searchAddr);
 }
--- src/main/java/org/didim365/hw1/repository/UserMapper.java
+++ src/main/java/org/didim365/hw1/repository/UserMapper.java
@@ -8,8 +8,32 @@ import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
+    /**
+     *
+     * @param email 사용자 아이디
+     * @return 조회된 유저
+     */
     Optional<User> checkDuplicateId(String email);
+
+    /**
+     *
+     * @param user 회원가입하기 위한 유저 객체
+     * @return 저장 성공여부
+     */
     int insertUser(User user);
+
+    /**
+     *
+     * @param userId 토큰검사를 위한 유저찾기
+     * @return 유저
+     */
     Optional<User> findByUser(String userId);
+
+    /**
+     * 아이디 찾기에서 입력받은 이메일, 이름
+     * @param email 입력받은 이메일
+     * @param name 입력받은 이름
+     * @return 이메일과 이름으로 조회된 아이디
+     */
     String findByUserId(String email, String name);
 }
