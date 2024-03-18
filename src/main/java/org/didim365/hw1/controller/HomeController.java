cf061622d226818ee4994c1b9e371c59981c0e2b
jungmin <jmpark@didim365.com>
2024-03-19 02:36:32 +0900

    home/user controller 주석 달기

--- src/main/java/org/didim365/hw1/controller/HomeController.java
+++ src/main/java/org/didim365/hw1/controller/HomeController.java
@@ -7,16 +7,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    /**
+     * 로그인 후 보이는 화면
+     * @return index html 파일
+     */
     @GetMapping("/")
     public String Home(){
         return "index";
     }
 
+    /**
+     * 도로명 검색 클릭 시 보이는 화면
+     * @return addrsearch(주소검색) html
+     */
     @GetMapping("/juso/search")
     public String AddrSearch(){
         return "addrsearch";
     }
 
+
+    /**
+     * 도로명 검색 -> 검색 -> 검색 리스트 클릭 후 보이는 지도 화면
+     * @return 검색 후 리스트 항목 클릭 시 지도 로드
+     */
     @GetMapping("/juso/search/map")
     public String Search(@RequestParam(name="roadAddr")String roadAddr,
                          @RequestParam(name="jibunAddr")String jibunAddr,
@@ -28,9 +41,15 @@ public class HomeController {
         return "showmap";
     }
 
+    /**
+     * 사용자 검색 내역 관리 화면
+     * @return 유저 검색 리스트 내역 html
+     */
     @GetMapping("/juso/search/history")
     public String SearchAddrHis(){
         return "hissearchjuso";
     }
+
+
 
 }
--- src/main/java/org/didim365/hw1/controller/UserController.java
+++ src/main/java/org/didim365/hw1/controller/UserController.java
@@ -22,33 +22,60 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     private final UserService userService;
 
+    /**
+     * 로그인 페이지에 접속할 때 호출
+     * @return 로그인 페이지를 나타내는 뷰의 이름 ("login.html")
+     */
     @GetMapping("/login")
     public String login(){
         return "login";
     }
 
+    /**
+     * 회원가입 페이지
+     * @return 회원가입 뷰 이름("signup.html")
+     */
     @GetMapping("/signup")
     public String signUpPage() {
         return "signup";
     }
 
+    /**
+     * signup.html에서 회원가입 버튼 클릭 시 호출
+     * @param signUpRequestDto 회원가입 요청에 필요한 정보를 담고 있는 DTO 객체
+     * @return 회원가입이 성공적으로 처리되었을 경우 201 Created 상태 코드와 메시지를 포함한 응답 엔터티를 반환
+     */
     @PostMapping("/signup")
     public ResponseEntity signup(@RequestBody SignUpRequestDto signUpRequestDto){
-        log.info(signUpRequestDto.toString());
         userService.signup(signUpRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("회원가입에 성공하였습니다.");
     }
 
+    /**
+     * signup.html에서 중복확인 버튼 클릭 시 호출
+     * 아이디 중복 여부를 확인
+     * @param userId 사용자가 확인하고자 하는 아이디
+     * @return 아이디 중복 확인 결과를 담은 ResponseEntity 객체 반환
+     */
     @PostMapping("/duplication/id/check")
     public ResponseEntity checkdDuplicateId(@RequestBody String userId){
         return ResponseEntity.ok(userService.checkDuplicateId(userId));
     }
 
+    /**
+     * 아이디 찾기 페이지
+     * @return 아이디 찾기 페이지("finduserId")
+     */
     @GetMapping("/help/idInquiry")
     public String findUserId(){
         return "finduserId";
     }
 
+    /**
+     * 아아디 찾기 버튼 클릭시 호출
+     * @param findUserRequestDto 입력받은 이메일과 이름 정보를 담고 있는 DTO 객체
+     * @return 찾은 아이디를 ResponseEntity 객체에 담아 반환
+     */
     @PostMapping("/find/userid")
     public ResponseEntity<FindUserResponseDto> findUserId(@RequestBody FindUserRequestDto findUserRequestDto){
         return ResponseEntity.ok(userService.findUserId(findUserRequestDto.getEmail(), findUserRequestDto.getName()));
